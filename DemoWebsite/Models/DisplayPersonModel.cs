c479cf8f8dfe4c0b72ede5b970c2fab7c36e44e0
weijie <weijie.yap@enfrasys.com>
2021-08-17 17:59:07 +0800

    Update People page

---
+++ DemoWebsite/Models/DisplayPersonModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebsite.Models
+{
+    public class DisplayPersonModel
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Name is too long.")]
+        [MinLength(3, ErrorMessage = "Name is too short.")]
+        public string name { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Category is too long.")]
+        [MinLength(5, ErrorMessage = "Category is too short.")]
+        public string category { get; set; }
+    }
+}
